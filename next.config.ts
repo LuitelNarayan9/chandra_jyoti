3f0397d87caa7879c69296b94cc7101fcab35347
Narayan Luitel <nluitel99@gmail.com>
2026-02-18 18:26:23 +0530

    Step 1.3: Authentication - Clerk setup, sign-in/sign-up pages, webhook handler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['decisive-subneural-serafina.ngrok-free.dev']
 };
 
 export default nextConfig;
